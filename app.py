8d1db00a240c6a6051cade01ee27d9d73b7cde6f
shobith-0103 <shobithnwork@gmail.com>
2026-01-29 12:53:01 +0530

    added app.py

--- app.py
+++ app.py
@@ -81,4 +81,5 @@ def home():
     return render_template_string(html)
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
+
